e191d62c22a295a16a5e54d7fa50b28fa3f587cb
Noah Lambe <noahalambe@gmail.com>
2025-01-09 12:42:38 -0330

    changed logic from if statement to switch

--- src/LabTask1.java
+++ src/LabTask1.java
@@ -7,28 +7,39 @@ public class LabTask1 {
         System.out.println("Enter a number (0-9): ");
         int num1 = sc.nextInt();
 
-        if (num1 > 9) {
-            System.out.println("Invalid input. Please enter a number between 0 and 9.");
-        } else if (num1 == 0) {
-            System.out.println("Zero.");
-        } else if (num1 == 1) {
-            System.out.println("One.");
-        } else if (num1 == 2) {
-            System.out.println("Two.");
-        } else if (num1 == 3) {
-            System.out.println("Three.");
-        } else if (num1 == 4) {
-            System.out.println("Four.");
-        } else if (num1 == 5) {
-            System.out.println("Five.");
-        } else if (num1 == 6) {
-            System.out.println("Six.");
-        } else if (num1 == 7) {
-            System.out.println("Seven.");
-        } else if (num1 == 8) {
-            System.out.println("Eight.");
-        } else if (num1 == 9) {
-            System.out.println("Nine.");
+        switch (num1) {
+            case 0:
+                System.out.println("Zero.");
+                break;
+            case 1:
+                System.out.println("One.");
+                break;
+            case 2:
+                System.out.println("Two.");
+                break;
+            case 3:
+                System.out.println("Three.");
+                break;
+            case 4:
+                System.out.println("Four.");
+                break;
+            case 5:
+                System.out.println("Five.");
+                break;
+            case 6:
+                System.out.println("Six.");
+                break;
+            case 7:
+                System.out.println("Seven.");
+                break;
+            case 8:
+                System.out.println("Eight.");
+                break;
+            case 9:
+                System.out.println("Nine.");
+                break;
+            default:
+                System.out.println("Invalid input. Please enter a number between 0 and 9.");
         }
 
         sc.close();
